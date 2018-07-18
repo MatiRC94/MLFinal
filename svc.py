17a21858acbf2970771975e24de36f95e6802323
MatiRC94 <matisaper@hotmail.com>
2018-07-18 20:23:01 -0300

    muchisimos cambios

--- svc.py
+++ svc.py
@@ -13,7 +13,7 @@ from sklearn.utils import shuffle
 #HARDCODEADO
 inputs = []
 classes = []
-kfolds = 2
+kfolds = 5
 
 
 
@@ -36,9 +36,25 @@ with open(INPUTS, newline='') as f:
         inputs.append(list(map(lambda x : float(x), row[:-1])))
         classes.append(int(row[-1]))
         
-#clf = svm.SVC()
+#clf = svm.SVC(kernel=KERNEL, C=PARAM_C)
 #clf.fit(inputs, classes)
 #print(clf)
+
+flatten = lambda l: [item for sublist in l for item in sublist]
+
+def errorCalc (predictval,realval):
+    return
+
+#errorCalc [] [] n c         = c
+#errorCalc (x:xs) (y:ys) n c = errorCalc xs ys n c if x===y else errorCalc xs ys n ((c+1)/n)
+
+def errorCalc(x,y) :
+    error=0
+    for i,j in x,y:
+        if (i!=j):
+            error= error+1    
+            
+    return error/len(x)
 
 
 def kfoldFitting (data,clases,ival):
@@ -50,20 +66,49 @@ def kfoldFitting (data,clases,ival):
     valSet = data[ival]
     # Borro inplace el conjunto de validacion 
     del tempData[ival]
+    del tempClas[ival]
+    tempClas = flatten(tempClas)
+    tempData = flatten(tempData)
+    clf = svm.SVC()
+    clf.fit(tempData, tempClas)
+
+    print(data[ival])
+    print(clases[ival])
+
+    prediction=clf.predict(data[ival])
+    print ("prediction:")
+    print(prediction)
+    return(errorCalc (clases[ival],prediction))
     
 
+## MAIN
 
 
 
 #shuffle 
 shuffleInputs, shuffleClass = shuffle(inputs,classes)
 
+print("inputs crudos: ")
+print(inputs)
+
+print("clases crudos: ")
+print(classes)
 
 #print(chunkAll(shuffleInputs, shuffleClass,kfolds))
-print(chunkAll(shuffleInputs,shuffleClass,kfolds))
+print("Chunkeado: ")
+(chunkdata,chunkclass) =chunkAll(shuffleInputs,shuffleClass,kfolds)
+
+print((chunkdata,chunkclass))
 
 
 
+kfoldFitting(chunkdata,chunkclass, 0)
+
+def createFiles (i)
+    file = open("fold"+i+".data","w") 
+
+    file.write("")
+    file.close() 
 
 
 
